4ddb673d295629645df8a54e5a307346fa09066b
Digl7 <dgl1997@gmail.com>
2020-12-09 08:45:37 +0100

    push desde el instituto

--- js/script.js
+++ js/script.js
@@ -3,3 +3,29 @@ function colorAmarillo(){
 
 }
 
+
+function Light() {
+    document.body.style.background = "white";
+    document.getElementById('main-nav').style.background ="white";
+    document.getElementById('main-nav').style.color ="#181B1D";
+    document.getElementById('ordenar').style.background ="white";
+    document.getElementById('ordenar').style.color ="#181B1D";
+}
+
+  
+function Dark() {
+    document.body.style.background = "#181B1D";
+    document.getElementById('main-nav').style.background ="#181B1D";
+    document.getElementById('main-nav').style.color ="white";
+    document.getElementById('ordenar').style.background ="#181B1D";
+    document.getElementById('ordenar').style.color ="white";
+}
+
+function menu() {
+  var x = document.getElementById("myLinks"); 
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
